ce0dd6fab9b882125b74184ea0fd36465f6253e9
AliLop”git config --global user.email alicia.hl87@gmail.comgit config --global user.name AliLop” <65405578+AliLop@users.noreply.github.com>
2020-11-23 20:38:00 +0000

    done

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+function Footer() {
+    return ( 
+        <div class="row">
+            <div class="col-sm-3">
+            <img class="icon" src="/images/icon1.png" /> 
+            <h4> Declarative </h4> 
+            <p>React makes it painless to create interactive UIs.</p>  
+            </div>
+            <div class="col-sm-3">
+            <img class="icon" src="/images/icon2.png" /> 
+            <h4> Components </h4> 
+            <p>Build encanpsulated components that manage their state.</p> 
+            </div>
+            <div class="col-sm-3">
+            <img class="icon" src="/images/icon3.png" /> 
+            <h4> Single-Way </h4> 
+            <p>A set of immutable values are passed to the component´s.</p>  
+            </div>
+            <div class="col-sm-3">
+            <img class="icon" src="/images/icon4.png" /> 
+            <h4> JSX </h4> 
+            <p>Statically-typed, desinged to run on modern browsers.</p>  
+            </div>
+           
+       
+
+       </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+function Header() {
+    return ( 
+        <div>
+        <nav class="navbar">
+            <img class="logo" src="/images/ironhack-logo.svg" /> 
+            <img class="bars" src="/images/menu-top.svg" /> 
+        </nav> 
+        <div class="main">
+                <h1><strong>Say hello to <br/>
+                ReactJS </strong></h1> 
+                
+                <p class="subtitle"> You will learn how to use<br/>
+                the most popular frontend library,<br/>
+                and become a super Ninja developer
+                </p>
+
+                <button><strong> Awesome! </strong></button>
+        </div>
+       </div>
+    )
+}
+
+export default Header;
